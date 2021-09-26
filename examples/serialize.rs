f3a81f509caaef5a008b85cb106cec68e44bb62e
Yiyuan Liu <yiyuanliu1997@gmail.com>
2021-09-26 21:20:35 +0800

    cargo fmt

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -17,19 +17,19 @@ mod serde {
             id: 9527,
             parents: parents.to_owned(),
         };
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let mut flexbuffer = flexbuffers::FlexbufferSerializer::default();
             student.serialize(&mut flexbuffer).unwrap();
         }
         let dur_serialize = begin.elapsed().unwrap();
-    
+
         let mut flexbuffer = flexbuffers::FlexbufferSerializer::default();
         student.serialize(&mut flexbuffer).unwrap();
         let data = flexbuffer.take_buffer();
         let data_len = data.len();
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let student1 = flexbuffers::from_slice::<Student>(data.as_slice()).unwrap();
@@ -51,17 +51,17 @@ mod serde {
             id: 9527,
             parents: parents.to_owned(),
         };
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let data = bincode::serialize(&student).unwrap();
             assert!(data.len() != 0);
         }
         let dur_serialize = begin.elapsed().unwrap();
-    
+
         let data = bincode::serialize(&student).unwrap();
         let data_len = data.len();
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let student1: Student = bincode::deserialize(&data[..]).unwrap();
@@ -78,7 +78,12 @@ mod serde {
 }
 
 mod rkyv {
-    use rkyv::{Archive, Serialize, archived_root, ser::{Serializer, serializers::AllocSerializer}, with::Boxed};
+    use rkyv::{
+        archived_root,
+        ser::{serializers::AllocSerializer, Serializer},
+        with::Boxed,
+        Archive, Serialize,
+    };
 
     #[derive(Archive, Serialize)]
     struct Parent<'a> {
@@ -98,27 +103,27 @@ mod rkyv {
         parents: &'a [Parent<'a>],
     }
 
-    pub fn benchmark(name: &str, parents: & [&str]) {
+    pub fn benchmark(name: &str, parents: &[&str]) {
         let student = Student {
             name,
             age: 26,
             id: 9527,
             parents: unsafe { std::mem::transmute(parents) },
         };
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let mut serializer = AllocSerializer::<64>::default();
             serializer.serialize_value(&student).unwrap();
         }
         let dur_serialize = begin.elapsed().unwrap();
-    
+
         let mut serializer = AllocSerializer::<64>::default();
         assert_eq!(serializer.pos(), 0);
         serializer.serialize_value(&student).unwrap();
         let data_len = serializer.pos();
         let data = serializer.into_serializer().into_inner();
-    
+
         let begin = std::time::SystemTime::now();
         for _ in 0..super::ITERATIONS {
             let student1 = unsafe { archived_root::<Student>(&data[0..data_len]) };
